69d98c07c0138a9a62f74a0f3b32f5d592a58f7b
AdamStrzyzewski <36886348+AdamStrzyzewski@users.noreply.github.com>
2024-02-08 14:30:06 +0000

    Deploying to gh-pages from @ AdamStrzyzewski/test-qaa5@340204629dd1344303f3a50f03606ec06014c263 🚀

---
+++ task-02.ea967511.js
@@ -0,0 +1,2 @@
+const e=["Potatoes","Mushrooms","Garlic","Tomatos","Herbs","Condiments"].map((e=>{const t=document.createElement("li");return t.textContent=e,t.classList.add("item"),t}));document.querySelector("#ingredients").append(...e);
+//# sourceMappingURL=task-02.ea967511.js.map
